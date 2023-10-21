b844cada9e2af1495beb53b2fd532d588fe458a8
zzzzDenzzzz <12den24@gmail.com>
2023-10-21 10:03:54 +0300

    fix movie.cshtml

--- Movie/TagHelpers/EmailLinkTagHelper.cs
+++ Movie/TagHelpers/EmailLinkTagHelper.cs
@@ -2,12 +2,9 @@
 
 namespace Movie.TagHelpers
 {
-
-
     [HtmlTargetElement("a")]
     public class EmailLinkTagHelper : TagHelper
     {
-
         public string Email { get; set; }
         public string MyTitle { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
@@ -18,16 +15,6 @@ namespace Movie.TagHelpers
                 output.Content.Append(MyTitle ?? Email);
                 output.Attributes.Add("href", $"mailto:{Email}");
             }
-
-
-            ////string link = $"<a href=\"mailto:{mail}\">{title ?? mail}</a>";
-            //output.TagName = "a";
-            ////output.Content.Append("Youtube");
-            ////output.Attributes.Add("href", "https://www.youtube.com");
-
-
-            //output.Content.Append(MyTitle ?? Email);
-            //output.Attributes.Add("href", $"mailto:{Email}");
         }
     }
 }
